d28eb0760bd36a2c0d819e36a35cf173fa501ea4
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2018-01-07 21:25:49 +0100

    Processing 6 exemple

--- daniel/processing/6/1.js
+++ daniel/processing/6/1.js
@@ -1,15 +1,41 @@
-var img;
-function preload(){
-	img = loadImage("../assets/vdc-logo.png");
-}
+
 function setup(){
 	createCanvas( window.innerWidth,window.innerHeight, WEBGL);
-	background(51);
 }
 function draw(){
-	background(51);
+
 	rotateX(frameCount * 0.01);
 	rotateY(frameCount * 0.01);
-	texture(img);
-	box(200, 200, 200);
+	beginShape(QUADS);
+	fill(0, 1, 1); vertex(-100,  100,  100);
+	fill(1, 1, 1); vertex( 100,  100,  100);
+	fill(1, 0, 1); vertex( 100, -100,  100);
+	fill(0, 0, 1); vertex(-100, -100,  100);
+
+	fill(1, 1, 1); vertex( 100,  100,  100);
+	fill(1, 1, 0); vertex( 100,  100, -100);
+	fill(1, 0, 0); vertex( 100, -100, -100);
+	fill(1, 0, 1); vertex( 100, -100,  100);
+
+	fill(1, 1, 0); vertex( 100,  100, -100);
+	fill(0, 1, 0); vertex(-100,  100, -100);
+	fill(0, 0, 0); vertex(-100, -100, -100);
+	fill(1, 0, 0); vertex( 100, -100, -100);
+
+	fill(0, 1, 0); vertex(-100,  100, -100);
+	fill(0, 1, 1); vertex(-100,  100,  100);
+	fill(0, 0, 1); vertex(-100, -100,  100);
+	fill(0, 0, 0); vertex(-100, -100, -100);
+
+	fill(0, 1, 0); vertex(-100, 100, -100);
+	fill(1, 1, 0); vertex( 100,  100, -100);
+	fill(1, 1, 1); vertex( 100,  100,  100);
+	fill(0, 1, 1); vertex(-100,  100,  100);
+
+	fill(0, 0, 0); vertex(-100, -100, -100);
+	fill(1, 0, 0); vertex( 100, -100, -100);
+	fill(1, 0, 1); vertex( 100, -100,  100);
+	fill(0, 0, 1); vertex(-100, -100,  100);
+
+	endShape();
 }
