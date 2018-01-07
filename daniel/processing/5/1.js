a07a7f240a13134b383548d454cdfda0bf1c7cee
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2018-01-07 20:23:17 +0100

    Processing image 3D

--- daniel/processing/5/1.js
+++ daniel/processing/5/1.js
@@ -2,21 +2,18 @@ var halfWidth = window.innerWidth * 0.5;
 var halfHeight = window.innerHeight * 0.5;
 var img;
 
-function preload() {
-
-}
-
 function setup() {
-	createCanvas( window.innerWidth,window.innerHeight);
-	//background(51);
+	createCanvas( window.innerWidth,window.innerHeight, WEBGL);
+	background(51);
 	img = loadImage("../assets/vdc-logo.png");
 	
 }
 
 function draw() {
 	image(img, 0, 0);
-	/*background(51);
+	background(51);
  	rotateX(frameCount * 0.01);
   	rotateY(frameCount * 0.01);
-  	box(200, 200, 200);*/
+  	texture(img);
+  	box(200, 200, 200);
 }
