66ebae5f2a4a02c29e709cda7bf521ad4802474f
dsengmanivanh <daniel.sengmanivanh@gmail.com>
2018-01-07 20:33:37 +0100

    Processing image 3D

--- daniel/processing/5/1.js
+++ daniel/processing/5/1.js
@@ -1,12 +1,12 @@
 var img;
 function preload(){
-  img = loadImage(“../assets/vdc-logo.png”);
+  img = loadImage("../assets/vdc-logo.png");
 }
 function setup(){
   createCanvas(500,500,WEBGL);
 }
 function draw(){
-  background(255);
+ /* background(255);
   texture(img);
-  box(45);
+  box(45);*/
 }
